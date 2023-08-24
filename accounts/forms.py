33d1d47194e1d9da6389f76f88bee4bbf4aba7d5
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2023-08-24 15:09:49 -0300

    edit and editpassword created

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,3 +22,18 @@ class RegisterForm(UserCreationForm):
         if commit:
             user.save()
         return user
+    
+class EditAccountsForm(forms.ModelForm):
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        queryset = User.objects.filter(
+            email=email).exclude(id=self.instance.id)
+
+        if queryset.exists():
+            raise forms.ValidationError('Já existe usuario com este Email.')
+        return email
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.conf import settings
 from django.contrib.auth import authenticate, login
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountsForm
 
 def register(request):
     template_name = 'register.html'
@@ -26,8 +26,33 @@ def register(request):
 
 @login_required()
 def edit(request):
-    pass
+    template_name = 'edit.html'
+    context = {}
+    if request.method == 'POST':
+        form = EditAccountsForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditAccountsForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditAccountsForm(instance=request.user)
+    context['form'] = form
+    return render(request,template_name, context)
 
 @login_required()
 def profile(request):
     return render(request, 'profile.html')
+
+@login_required()
+def editpassword(request):
+    template_name = 'editpassword.html'
+    context = {}
+    if request.method == 'POST':
+        form = PasswordChangeForm(data = request.POST , user = request.user)
+        if form.is_valid():
+            form.save()
+            context['success'] = True
+    else:
+        form = PasswordChangeForm(user = request.user)
+    context['form'] = form
+    return render(request,template_name, context)
